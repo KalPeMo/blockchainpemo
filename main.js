572457e8e15bfa58d5b2ed7b2dd311ca9e5e918c
KalPeMo <96583745+KalPeMo@users.noreply.github.com>
2022-07-28 20:47:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,69 @@
-//npm install crypto-js--save
+const SHA256 = require('crypto-js/sha256');
 
-const SHA256=require('crypto-js/sha256')
+class Bloque {
+    constructor(indice, dato, hashAnterior = ''){
+        this.indice = indice;
+        this.fecha = new Date();
+        this.dato = dato;
+        this.hashAnterior = hashAnterior;
+        this.hash = this.crearHash();
+        this.nonce = 0;
+    }
 
-class bloque{
-	constructor(indice,dato,hashAnterior='') {
-	this.indice=indice;
-	this.fecha=new Date();
-	this.dato=dato;
-	this.hashAnterior=hashAnterior
-	this.hash=this.crearHash();
-	this.nonce=0;
-	}
-	
-	crearHash(){
-	return SHA256(this.index + this.fecha + this.dato+this.hashAnterior+this.nonce).toString();
-	}
-	
-	minar(dificultad){
-			while (!this.hash.startsWith(dificultad)) 	{
-									//Mientras no se empiece por ña dificultad propuesta, no inicia
-									this.nonce++;
-									this.hash=this.crearHash;
-									}
-				
-			}
+    crearHash() {
+        return SHA256(this.indice + this.fecha + this.dato + this.hashAnterior + this.nonce).toString();
+    }
+
+    mine(dificultad) {
+        while(!this.hash.startsWith(dificultad)) {
+            //Mientras no se empiece por la dificultad propuesta, no inicia, es lo que deberìa ser
+            this.nonce++;
+            this.hash = this.crearHash();
+        }
+    }
 }
 
-class blockChain	{
-	constructor(genesis,  dificultad='0')	{
-		
-				this.cadena=[this.crearPrimerBloque(genesis)];
-				this.dificultad=dificultad;
-				}
-		crearPrimerBloque(genesis)	{
-						return new bloque(0,genesis);
-						}
-		getUltimoBloque()	{
-					return this.cadena[this.cadena.length-1];
-					}
-		addBloque(dato)	{
-				let antBloque = this.getUltimoBloque();
-				let bloque= new bloque(antBloque.index+1), dato, anteBloque.hash);
-				bloque.minar(this.dificultad);
-				console.log('El bloque se ha minado con éxito'+bloque.hash+' con nonce'+bloque.nonce);
-				this.cadena.push(bloque);
-				
-				}
+class BlockChain {
+    constructor(genesis, dificultad = '00'){
+        this.cadena = [this.crearPrimerBloque(genesis)]; 
+        this.dificultad =dificultad ;
+    }
+    crearPrimerBloque(genesis){
+        return new Bloque(0,genesis);
+    }
+    getUltimoBloque() {
+        return this.cadena[this.cadena.length-1];
+    }
+    addBloque(dato){
+        let bloqueAnterior= this.getUltimoBloque();
+        let bloque = new Bloque(bloqueAnterior.indice+1, dato, bloqueAnterior.hash);
+        bloque.mine(this.dificultad);
+        console.log('El bloque se ha minado con éxito '+bloque.hash+' con un nonce de '+bloque.nonce);
+        this.cadena.push(bloque);
+    }
+    isValid() {
+        for(let i=1; i < this.cadena.length; i++){
+            let bloqueAnterior = this.cadena[i-1];
+            let bloqueActual = this.cadena[i];
 
-				}
+            if(bloqueActual.hashAnterior != bloqueAnterior.hash)
+                return false;
+            
+            if(bloqueActual.crearHash() != bloqueActual.hash)
+                return false;
+        }
+        return true;
+    }
+}
+
+
+let miMoneda = new BlockChain('Información de genesis','00');
+//modificar aqui el valor maximo del step para obtener la cantidad de bloque deseada
+for (let step = 0; step < 5; step++) {
+    miMoneda.addBloque('En la cadena este es el bloque # '+step);
+    }
+console.log(JSON.stringify(miMoneda.cadena,null,2));
+
 
 //Prueba 1
 //block = new bloque(0,'prueba1')
